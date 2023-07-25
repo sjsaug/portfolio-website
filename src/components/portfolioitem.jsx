799c744b69b5a05dea091165a222a2759d366cbb
sjsaug <sjsaug@gmail.com>
2023-07-25 15:33:24 -0400

    Add portfolio item and create requirement

---
+++ src/components/portfolioitem.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+function PortfolioItem() {
+    return (
+        <div>Portfolio</div>
+    )
+}
+
+export default PortfolioItem;
---
+++ src/data/portfolio.js
@@ -0,0 +1,20 @@
+export default [
+    {
+        title: "Codeguesser",
+        imgUrl: "",
+        stack: ["Python"],
+        link: "augustrm.me",
+    },
+    {
+        title: "Developer Portfolio",
+        imgUrl: "",
+        stack: ["React", "Tailwind", "Vite"],
+        link: "github.com/sjsaug/codeguesser",
+    },
+    {
+        title: "N/A",
+        imgUrl: "",
+        stack: ["N/A"],
+        link: "N/A",
+    },
+]
