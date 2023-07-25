92c531f192a56b1faee2ed9077de0d50c534c381
sjsaug <sjsaug@gmail.com>
2023-07-25 15:44:03 -0400

    Add portfolio project system

--- src/components/portfolioitem.jsx
+++ src/components/portfolioitem.jsx
@@ -1,8 +1,21 @@
 import React from "react";
 
-function PortfolioItem() {
+function PortfolioItem({title, imgUrl, stack, link}) {
     return (
-        <div>Portfolio</div>
+        <div>
+        <img src={imgUrl} 
+        alt="portfolio"
+        className="w=full h-36 md:h-48 object-cover cursor-pointer"
+        />
+        <div>className="w-full p-4"
+            <h3>{title}</h3>
+            <p>{stack.map(item =>
+                <span>
+                    {item}
+                </span>
+                )}</p>
+        </div>
+        </div>
     )
 }
 
