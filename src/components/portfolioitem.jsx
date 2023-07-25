51ce184069b31828f6b5d41b8f87eb3b21210344
sjsaug <sjsaug@gmail.com>
2023-07-25 15:58:33 -0400

    remove azure deployment (for now)

--- src/components/portfolioitem.jsx
+++ src/components/portfolioitem.jsx
@@ -8,7 +8,7 @@ function PortfolioItem({title, imgUrl, stack, link}) {
         className="w=full h-36 md:h-48 object-cover cursor-pointer"
         />
         <div>className="w-full p-4"
-            <h3>{title}</h3>
+            <h3 className="text-lg md:text-xl mb-2 md:mb-3 font-semibold">{title}</h3>
             <p>{stack.map(item =>
                 <span>
                     {item}
