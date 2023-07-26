23f998e29981a89c7064fc2f80a44a3373691f1f
sjsaug <sjsaug@gmail.com>
2023-07-26 01:31:18 -0400

    fix images

--- src/data/portfolio.js
+++ src/data/portfolio.js
@@ -7,13 +7,13 @@ export default [
     },
     {
         title: "Developer Portfolio",
-        imgUrl: "public/assets/dev_portfolio_preview.png",
+        imgUrl: "/assets/dev_portfolio_preview.png",
         stack: ["React", "Tailwind", "Vite"],
         link: "https://augustrm.me",
     },
     {
         title: "GPT-TTS Chatbot",
-        imgUrl: "public/assets/gpt_tts_chatbot_preview.png",
+        imgUrl: "/assets/gpt_tts_chatbot_preview.png",
         stack: ["Python", "OpenAI API"],
         link: "https://github.com/sjsaug/openai_ttsbot",
     },
