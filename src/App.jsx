538ccecf8e55942ee446576dc735397c312de76d
sjsaug <sjsaug@gmail.com>
2023-07-26 03:40:09 -0400

    Reposition theme button

--- src/App.jsx
+++ src/App.jsx
@@ -69,7 +69,7 @@ function App() {
     <button
     type="button"
     onClick={handleThemeSwitch}
-    className="fixed p-2 z-10 right-20 top-4 bg-violet-300 text-lg p-1 rounded-md dark:bg-orange-300"
+    className="fixed p-2 z-10 right-5 top-4 bg-violet-300 text-lg p-1 rounded-md dark:bg-orange-300"
     >
       {theme === "dark" ? sun : moon}
     </button>
