8b36161ff2f12b80cca7a83c07ad26c8a4463388
sjsaug <sjsaug@gmail.com>
2023-09-02 21:22:34 -0400

    update intro

--- src/components/intro.jsx
+++ src/components/intro.jsx
@@ -5,9 +5,17 @@ function Intro() {
         <div className="flex items-center justify-center flex-col text-center pt-20 pb-6">
             <h1 className="text-4xl md:text-7xl mb-1 md:mb-3 font-bold dark:text-white">August</h1>
             <p className="text-base md:text-xl mb-3 font-medium">Computer Science Student</p>
-            <p className="text-sm max-w-xl mb-6 font-bold">Hi! My name is August and starting in September I will be studying Computer Science in Waterloo.
+            <p className="text-sm max-w-xl mb-6 font-bold">Hi! My name is August and I am studying Computer Science in Waterloo.
             I started my development journey three years ago developing Lua scripts and moved on to developing .NET applications in C++. For the past year I've been learning Python,
-            and right now I'm interested in web development. You can view the source code for this website along with many of my other projects on my {""} 
+            and right now I'm interested in web development. You can view the {""} 
+            <a
+                href="https://github.com/sjsaug/portfolio-website"
+                target="_blank"
+                className="text-blue-600 hover:underline underline-offset-2 decoration-2 decoration-red-600"
+                rel="noreferrer noopener"
+                >
+                source code
+                </a> {""} for this website along with many of my other projects on my {""} 
             <a
                 href="https://github.com/sjsaug"
                 target="_blank"
